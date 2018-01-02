3387fd6d4248966c126f88d9fc9515c88cfd81da
Michael O'Ryan <michael.oryan@gmail.com>
2018-01-02 18:43:29 +1100

    Google Style

--- double.cpp
+++ double.cpp
@@ -7,57 +7,60 @@
 // Function prototypes
 
 template<class T>
-T doubleNumber(T number);
+T DoubleNumber(T number);
 
 template<class T>
-bool doublingWillOverflow(T number);
+bool IsDoubleOverflow(T number);
 
 template<class T>
-bool doublingWillUnderflow(T number);
+bool IsDoubleUnderflow(T number);
 
 template<class T>
-T halveNumber(T number);
+T HalveNumber(T number);
 
 template<class T>
-bool inRange(T number, T min, T max);
+bool IsInRange(T number, T min, T max);
 
 template<class T>
-void outputDoubleNumber(T number, std::ostream & out = std::cout);
+void OutputDoubleNumber(T number, std::ostream & out = std::cout);
 
-void promptUserForNumber(std::ostream & out = std::cout);
-
-template<class T>
-void promptValidNumberRange(T min, T max, std::ostream & out = std::cout);
+void PromptUserForNumber(std::ostream & out = std::cout);
 
 template<class T>
-void readAndDoubleNumber(std::istream & in = std::cin, std::ostream & out = std::cout);
+void PromptValidNumberRange(T min, T max, std::ostream & out = std::cout);
 
 template<class T>
-T readInt(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max(), std::istream & in = std::cin);
+void ReadAndDoubleNumber(std::istream & in = std::cin,
+                         std::ostream & out = std::cout);
 
-void removeLine(std::istream & in = std::cin);
+template<class T>
+T ReadInt(T min = std::numeric_limits<T>::min(),
+          T max = std::numeric_limits<T>::max(),
+          std::istream & in = std::cin);
+
+void RemoveLine(std::istream & in = std::cin);
 
 
 // Function Implementations
 
 int main() {
-    readAndDoubleNumber<long long int>(std::cin, std::cout);
+    ReadAndDoubleNumber<long long int>(std::cin, std::cout);
     
     return 0;
 }
 
 template<class T>
-void readAndDoubleNumber(std::istream & in, std::ostream & out) {
+void ReadAndDoubleNumber(std::istream & in, std::ostream & out) {
     try {
-        promptUserForNumber(out);
+        PromptUserForNumber(out);
         
-        T minimum_number = halveNumber(std::numeric_limits<T>::min());
-        T maximum_number = halveNumber(std::numeric_limits<T>::max());
+        T minimum_number = HalveNumber(std::numeric_limits<T>::min());
+        T maximum_number = HalveNumber(std::numeric_limits<T>::max());
         
-        T number = readInt(minimum_number, maximum_number, in);
-        T double_number = doubleNumber(number);
+        T number = ReadInt(minimum_number, maximum_number, in);
+        T double_number = DoubleNumber(number);
         
-        outputDoubleNumber(double_number, out);
+        OutputDoubleNumber(double_number, out);
     }
     catch(std::runtime_error e) {
         std::cerr << e.what() << std::endl;
@@ -72,58 +75,58 @@ void readAndDoubleNumber(std::istream & in, std::ostream & out) {
 }
 
 
-void promptUserForNumber(std::ostream & out) {
+void PromptUserForNumber(std::ostream & out) {
     out << "Enter a number to double: ";
 }
 
 template<class T>
-T halveNumber(T number) {
+T HalveNumber(T number) {
     return number / 2;
 }
 
 template<class T>
-T readInt(T min, T max, std::istream & in) {
+T ReadInt(T min, T max, std::istream & in) {
     T number;
     
     in >> number;
-    bool readNumberSucceeded = in.good();
+    bool read_number_succeeded = in.good();
     
-    while(!readNumberSucceeded || !inRange(number, min, max)) {
+    while (!read_number_succeeded || !IsInRange(number, min, max)) {
         in.clear();
         
-        removeLine(in);
+        RemoveLine(in);
         
-        promptValidNumberRange(min, max);
+        PromptValidNumberRange(min, max);
 
         in >> number;
-        readNumberSucceeded = in.good();
+        read_number_succeeded = in.good();
     }
     
     return number;
 }
 
 template<class T>
-bool inRange(T number, T min, T max) {
-    return number >= min && number <= max;
+bool IsInRange(T number, T min, T max) {
+    return (number >= min) && (number <= max);
 }
 
-void removeLine(std::istream & in) {
+void RemoveLine(std::istream & in) {
     std::string ignored_chars;
     getline(in, ignored_chars);
 }
 
 template<class T>
-void promptValidNumberRange(T min, T max, std::ostream & out) {
-    out << "Please enter a number between " << min << " and " << max << ": ";;
+void PromptValidNumberRange(T min, T max, std::ostream & out) {
+    out << "Please enter a number between ";
+    out << min << " and " << max << ": ";
 }
 
 template<class T>
-T doubleNumber(T number) {
+T DoubleNumber(T number) {
     
-    if(doublingWillOverflow(number)) {
+    if (IsDoubleOverflow(number)) {
         throw std::overflow_error("Overflow error!");
-    } 
-    else if(doublingWillUnderflow(number)) {
+    } else if (IsDoubleUnderflow(number)) {
         throw std::underflow_error("Underflow error!");
     }
     
@@ -133,16 +136,17 @@ T doubleNumber(T number) {
 }
 
 template<class T>
-bool doublingWillOverflow(T number) {
-    return number > halveNumber(std::numeric_limits<T>::max());
+bool IsDoubleOverflow(T number) {
+    return number > HalveNumber(std::numeric_limits<T>::max());
 }
 
 template<class T>
-bool doublingWillUnderflow(T number) {
-    return number < halveNumber(std::numeric_limits<T>::min());
+bool IsDoubleUnderflow(T number) {
+    return number < HalveNumber(std::numeric_limits<T>::min());
 }
 
 template<class T>
-void outputDoubleNumber(T number, std::ostream & out) {
+void OutputDoubleNumber(T number, std::ostream & out) {
     out << "Double your number is " << number << std::endl;
 }
+
